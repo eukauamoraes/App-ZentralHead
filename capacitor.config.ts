b805c12d4062cceed277b7c2de640bd7d084bab7
Kaua <kauamoraes270@gmail.com>
2025-11-28 08:50:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-ZentralHead',
+  webDir: 'www'
+};
+
+export default config;
