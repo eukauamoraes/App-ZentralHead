488a8a06bf5686d16c8f888a5e8d8f654d8ab6c6
Nikizinha <nicolenayti@gmail.com>
2026-02-18 23:18:07 -0300

    TELAS (catalogo, checkout, detalhes_pedido, detalhesproduto) rascunho

---
+++ src/app/checkout/checkout.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chekout',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+  templateUrl: './chekout.page.html',
+  styleUrls: ['./chekout.page.scss'],
+})
+export class ChekoutPage {}
